847410ecbe286b8f805644075fe0ab1285992019
guilhermebodin <guilherme.b.moraes@gmail.com>
2019-01-25 15:03:30 -0200

    prettyurls = false

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,7 @@ makedocs(
     modules = [Documenter],
     format = Documenter.HTML(
         # Use clean URLs, unless built as a "local" build
-        prettyurls = !("local" in ARGS),
-        canonical = "MyDocs",
+        prettyurls = false
     ),
     assets = ["assets/logo.ico"],
     sitename = "Ensino Lamps",
